6e6bad54370f3d2f9d66a656c5d67bb4aec62e5c
Ajay Addepalle <32472214+ajay-addepalle@users.noreply.github.com>
2020-06-04 00:18:30 +0530

    updated keymap

--- keyboards/lily58/keymaps/ajay_addepalle/keymap.c
+++ keyboards/lily58/keymaps/ajay_addepalle/keymap.c
@@ -46,11 +46,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                             KC_LALT, KC_LGUI,LOWER, KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_RGUI \
+KC_A,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_MINS,\
+KC_DEL,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_GRV,\
+KC_BSPC,  LCTL_T(KC_A),  LALT_T(KC_S),  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  RALT_T(KC_L),  RCTL_T(KC_SCLN), KC_QUOT,\
+KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_ESC,  KC_TAB,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,\
+KC_LCTL,  KC_LALT,  MO(1),  KC_SPC,  KC_ENT,  MO(2),  KC_RGUI,  KC_RCTL \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -67,11 +67,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT( \
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,\
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, \
-  _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-                             _______, _______, _______, _______, _______,  _______, _______, _______\
+KC_TRNS,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_F11,\
+KC_TRNS,  KC_EXLM,  KC_AT,  KC_LCBR,  KC_RCBR,  KC_PIPE,  KC_PSLS,  KC_P7,  KC_P8,  KC_P9,  KC_PAST,  KC_F12,\
+KC_TRNS,  KC_HASH,  KC_DLR,  KC_LPRN,  KC_RPRN,  KC_BSLS,  KC_PMNS,  KC_P4,  KC_P5,  KC_P6,  KC_PPLS,  KC_TRNS,\
+KC_TRNS,  KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_SLSH,  KC_ESC,  KC_TAB,  KC_TRNS,  KC_P1,  KC_P2,  KC_P3,  KC_PEQL,  KC_TRNS,\
+KC_TRNS,  KC_APP,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_P0,  KC_PDOT \
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -89,11 +89,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-  KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, \
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-                             _______, _______, _______,  _______, _______,  _______, _______, _______ \
+KC_ACL2,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MPLY,  KC_MRWD,  KC_MFFD,  KC_MPRV,  KC_MNXT,  KC_MSTP,\
+KC_ACL1,  KC_TRNS,  KC_TRNS,  KC_MS_U,  KC_TRNS,  KC_HOME,  KC_PGUP,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_VOLU,\
+KC_ACL0,  KC_TRNS,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT,  KC_TRNS,  KC_VOLD,\
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_END,  KC_BTN3,  KC_TRNS,  KC_PGDN,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MUTE,\
+KC_TRNS,  KC_BTN2,  KC_TRNS,  KC_BTN1,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS \
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
